90a2f0fce8c0565143c4c828af9c4ef60356de08
davidcbk <dmedina@cubikosolutions.com>
2018-03-04 23:15:11 +0000

    v3

--- app/src/main/java/com/cubikosolutions/dampgl/ejemplopcpartes/TipoParteActivity.java
+++ app/src/main/java/com/cubikosolutions/dampgl/ejemplopcpartes/TipoParteActivity.java
@@ -3,21 +3,26 @@ package com.cubikosolutions.dampgl.ejemplopcpartes;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+
 
 import com.cubikosolutions.dampgl.ejemplopcpartes.parte.ParteActivity;
 
-public class TipoParteActivity extends Activity implements OnClickListener {
+public class TipoParteActivity extends Activity  implements OnClickListener {
 
     private Button btnRellenar;
     private RadioButton rdbEntrega, rdbTrabajo;
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_parte);
 
